3703b5736829ff53e7beb98fce4bd00c729b02db
Gagasbandas <kjjcds@gmail.com>
2024-03-19 23:24:04 +0900

    bandwidth가 다되면 vpn을 이용하도록 수정(WIP)

--- ehentai_mass_downloader.py
+++ ehentai_mass_downloader.py
@@ -11,6 +11,10 @@ import random
 from dotenv import load_dotenv
 import re
 import shutil
+import subprocess
+
+from tqdm import tqdm
+
 
 def sanitize_directory_name(name):
     # remove characters that windows does not allow
@@ -24,8 +28,15 @@ def read(url, args, s):
     # e.g) self.url = https://exhentai.org/((?f_search=.*)|(tag/.*))
     # create a log file
     now = datetime.datetime.now()
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
     log_dir = os.path.join(args.output, f'{now.strftime("%Y-%m-%d_%H-%M-%S")}-page{args.pages}.log')
     error_dir = os.path.join(args.output, f'{now.strftime("%Y-%m-%d_%H-%M-%S")}-page{args.pages}.error')
+    use_vpn = args.myvpn is not None
+    if use_vpn:
+        myvpn = os.listdir(args.myvpn)
+        myvpn = [os.path.join(args.vpn, vpn) for vpn in myvpn if vpn.endswith(".ovpn")]
+    vpn_connected = None
     with open(log_dir, "w", encoding='utf-8') as log, open(error_dir, "w", encoding='utf-8') as error:
         log.write(f"URL: {url}\n")
         log.write(f"Pages: {args.pages}\n")
@@ -33,42 +44,91 @@ def read(url, args, s):
         log.write(f"Filter: {args.filter}\n")
 
         pages = args.pages
-        print('Getting pages')
         page_soup = get_soup(url, s)
         page_number = 1
         while True:
             if page_number >= pages:
-                print(f"Getting articles from {page_number} page")
                 log.write(f"Getting articles from {page_number} page\n")
-                articles_list = get_articles_list(page_soup, s, args)
-                for article_and_tags in articles_list:
+                list_items = page_soup.find_all("td", class_="gl3c glname")
+                len_articles = len(list_items)
+                pbar = tqdm(list_items, total=len_articles, leave=False, position=0, unit="arts",
+                                    desc=f"{page_number} page : Title".ljust(60),
+                                    bar_format="{desc}{percentage:3.0f}%|{bar:25}{r_bar}")
+                for article in pbar:
                     # [soup, tags={"artist": ["artist1", "artist2"], "group": ["group1", "group2"], ...}]
                     # create directorys for each article_and_tags
                     # e.g) title
-                    title = get_title(article_and_tags[0])
+                    article_url = article.find("a")["href"]
+                    article_soup = get_soup(article_url, s)
+                    title = get_title(article_soup)
+                    desc = f"{page_number} page : {title}".ljust(60)[:60]
+                    pbar.set_description(desc)
+                    tags = get_tags(article_soup)
+                    is_valid = True
+                    if args.filter is not None:
+                        for key, value in args.filter.items():
+                            if key not in tags:
+                                continue
+                            if not any([v in tags[key] for v in value]):
+                                continue
+                            is_valid = False
+                            break
+
+                    if is_valid and args.tags is not None:
+                        for key, value in args.tags.items():
+                            if key in tags:
+                                if all([v in tags[key] for v in value]):
+                                    continue
+                            is_valid = False
+                            break
+                    if not is_valid:
+                        log.write(f"Skipping {title} because of the tags\n")
+                        continue
                     # some of the title will not be able to be used as a directory name
                     title = sanitize_directory_name(title)
                     directory = os.path.join(args.output, title)
-                    print("Saving " + title)
-                    log.write(f"Saving {title}\n")
                     # if the directory already exists, skip
                     if os.path.exists(directory):
-                        print(f"{title} already exists")
-                        log.write(f"{title} already exists\n")
                         continue
                     os.makedirs(directory)
                     log.flush()
 
-                    picture_pages = get_picture_pages(article_and_tags[0], s)
+                    log.write("Getting pictures from " + title + "\n")
+                    picture_pages = get_picture_pages(article_soup, s)
+                    len_pictures = len(picture_pages)
                     # save the pictures in a directory in a form of 1, 2, 3, ...
-                    save_tags(directory, article_and_tags[1])
-                    is_ok = save_pictures_from_pages_list(picture_pages, directory, s, error)
-                    if not is_ok:
-                        print("bandwidth exceeded")
-                        # remove the directory
-                        shutil.rmtree(directory)
-                        # Exit the program
-                        return
+                    log.write(f"Found {len_pictures} pictures\n")
+                    save_tags(directory, tags)
+                    idx = save_pictures_from_pages_list(title, picture_pages, directory, s, error, len_pictures - 1)
+                    if idx != -1:   # bandwidth exceeded
+                        log.write("bandwidth exceeded\n")
+                        # if use_vpn is False, just remove the directory and return
+                        if not use_vpn:
+                            shutil.rmtree(directory)
+                            return
+                        else:
+                            try:
+                                if vpn_connected is None:
+                                    connect_vpn = random.choice(myvpn)
+                                    VPN("connect", args.vpn, connect_vpn)
+                                    WaitUntilVPNConnected()
+                                    vpn_connected = connect_vpn
+                                else:
+                                    VPN("disconnect", args.vpn)
+                                    WaitUntilVPNDisconnected()
+                                    connect_vpn = random.choice(myvpn)
+                                    VPN("connect", args.vpn, connect_vpn)
+                                    WaitUntilVPNConnected()
+                                    vpn_connected = connect_vpn
+                                # retry the page
+                                idx = save_pictures_from_pages_list(title, picture_pages, directory, s, error, idx)
+                                if idx != -1:
+                                    raise Exception("VPN connection failed")
+                            except Exception as e:
+                                log.write("VPN connection failed with error: " + str(e) + "\n")
+                                shutil.rmtree(directory)
+                                return
+                pbar.close()
             # next page if exists
             # page = get_soup(page.find("a", {"id" : "dnext"})["href"])
             next = page_soup.find("a", {"id": "dnext"})
@@ -127,8 +187,6 @@ def get_articles_list(soup, s, args):
 
         if is_valid:
             articles_list.append([soup, tags])
-
-    print("Found " + str(len(articles_list)) + " articles on this page.")
     return articles_list
 
 def get_picture_pages(soup, s):
@@ -151,18 +209,19 @@ def get_picture_pages(soup, s):
         soup = get_soup(next_page, s)
     return pages_list
 
-def save_pictures_from_pages_list(urls, dir, s, log):
-    for i, url in enumerate(urls):
+def save_pictures_from_pages_list(title, urls, dir, s, log, start):
+    len_pics = start + 1
+    for i in tqdm(range(start, -1, -1), position=1, unit="pics", leave=False, total=len_pics):
+        url = urls[i]
         # get picture url from the page
         soup = get_soup(url, s)
         img_em = soup.find("img", {"id": "img"})
         ref = img_em["src"]
         if ref == "https://exhentai.org/img/509.gif":
             # log the bandwidth exceeded page
-            print(f"Bandwidth exceeded: {url} (i: {i})")
             log.write(f"Bandwidth exceeded: {url} (i: {i})\n")
             log.flush()
-            return None
+            return i
         ext = ref.split(".")[-1]
         # save the picture with requests.get(url)
         res = get_soup(ref, s, g_soup=False, image=True)
@@ -184,19 +243,16 @@ def save_pictures_from_pages_list(urls, dir, s, log):
                 ref = img_em["src"]
                 if ref == "https://exhentai.org/img/509.gif":
                     # log the bandwidth exceeded page
-                    print(f"Bandwidth exceeded: {url} (i: {i})")
                     log.write(f"Bandwidth exceeded: {url} (i: {i})\n")
-                    return None
+                    return i
                 res = get_soup(ref, s, g_soup=False, image=True)
                 if res is None:
                     # log the missing page
-                    print(f"Missing page: {url} (i: {i})")
                     log.write(f"Missing page: {url} (i: {i})\n")
                     log.flush()
                     continue
             else:
                 # log the missing page
-                print(f"Missing page: {url} (i: {i})")
                 log.write(f"Missing page: {url} (i: {i})\n")
                 log.flush()
                 continue
@@ -205,7 +261,7 @@ def save_pictures_from_pages_list(urls, dir, s, log):
         with open(os.path.join(dir, img_filename), 'wb') as f:
             f.write(res.content)
 
-        return True
+    return -1
 
 def arg_as_list(s):
     v = ast.literal_eval(s)
@@ -229,14 +285,6 @@ def arg_as_dict(s):
         new_dict[new_key] = new_values
     return new_dict
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Download from exhentai, e-hentai')
-    parser.add_argument('--url', '-u', type=str, help='url', required=True)
-    parser.add_argument('--pages', '-p', type=int, help='1', default=1)
-    parser.add_argument('--tags', '-t', type=arg_as_dict, help='{"artist": ["artist1", "artist2"], "group": ["group1", "group2"], ...}', default=None)
-    parser.add_argument('--filter', '-f', type=arg_as_dict, help='{"artist": ["artist1", "artist2"], "group": ["group1", "group2"], ...}', default=None)
-    parser.add_argument('--output', '-o', type=str, default='output')
-    return parser.parse_args()
 
 def session():
     load_dotenv()
@@ -244,10 +292,6 @@ def session():
     id = os.getenv("ID")
     pwd = os.getenv("PWD")
     session = os.getenv("SESSION")
-
-    print("ID: " + id, type(id))
-    print("PWD: " + pwd, type(pwd))
-    print("SESSION: " + session, type(session))
 
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0"}
     cookies = {'ipb_member_id': id, 'ipb_pass_hash': pwd, 'ipb_session_id': session}
@@ -273,6 +317,51 @@ def get_soup(url, s, g_soup=True, image=False):
     if g_soup:
             res = Soup(res.text, "html.parser")
     return res
+
+def VPN(action, vpn_path, myvpn=None):
+    valid_actions = ["connect", "disconnect", "reconnect", "status"]
+    if action not in valid_actions:
+        return
+    else:
+        if myvpn:
+            command = f"{vpn_path} --command {action} {myvpn}"
+        else:
+            command = f"{vpn_path} --command {action}"
+        subprocess.Popen(command, shell=True)
+
+def IPAddress():
+    ipconfig = os.popen('ipconfig').read()
+    ipv4 = re.findall(r'IPv4.*?(\d+\.\d+\.\d+\.\d+)', ipconfig)
+    return ipv4
+
+def WaitUntilVPNConnected():
+    s = time.time()
+    while len(IPAddress()) == 1:
+        time.sleep(3)
+        if (time.time() - s) > 15:
+            break
+    if len(IPAddress()) > 1:
+        time.sleep(5)
+
+def WaitUntilVPNDisconnected():
+    s = time.time()
+    while len(IPAddress()) > 1:
+        time.sleep(3)
+        if (time.time() - s) > 15:
+            break
+    if len(IPAddress()) == 1:
+        time.sleep(5)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Download from exhentai, e-hentai')
+    parser.add_argument('--url', '-u', type=str, help='url', required=True)
+    parser.add_argument('--pages', '-p', type=int, help='1', default=1)
+    parser.add_argument('--tags', '-t', type=arg_as_dict, help='{"artist": ["artist1", "artist2"], "group": ["group1", "group2"], ...}', default=None)
+    parser.add_argument('--filter', '-f', type=arg_as_dict, help='{"artist": ["artist1", "artist2"], "group": ["group1", "group2"], ...}', default=None)
+    parser.add_argument('--output', '-o', type=str, default='output')
+    parser.add_argument('--vpn', '-v', type=str, default='C:/Program Files/OpenVPN/bin/openvpn-gui.exe')
+    parser.add_argument('--myvpn', '-m', type=str, default=None)
+    return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_args()

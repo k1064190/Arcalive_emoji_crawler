c71287bad01a0bf266ab73d5488fece6cdd06ffc
Gagasbandas <kjjcds@gmail.com>
2024-03-20 00:19:01 +0900

    vpn test 파일

---
+++ vpn_test.py
@@ -0,0 +1,58 @@
+import os
+import re
+import time
+import subprocess
+
+def VPN(action, vpn_path, myvpn=None):
+    valid_actions = ["connect", "disconnect", "reconnect", "status"]
+    if action not in valid_actions:
+        return
+    else:
+        if myvpn:
+            command = f'"{vpn_path}" --command {action} {myvpn}'
+        else:
+            command = f'"{vpn_path}" --command {action}'
+        subprocess.Popen(command, shell=True)
+
+def IPAddress():
+    ipconfig = os.popen('ipconfig').read()
+    ipv4 = re.findall(r'IPv4.*?(\d+\.\d+\.\d+\.\d+)', ipconfig)
+    return ipv4
+
+def WaitUntilVPNConnected():
+    s = time.time()
+    while len(IPAddress()) == 1:
+        time.sleep(3)
+        if (time.time() - s) > 15:
+            break
+    if len(IPAddress()) > 1:
+        time.sleep(5)
+
+def WaitUntilVPNDisconnected():
+    s = time.time()
+    while len(IPAddress()) > 1:
+        time.sleep(3)
+        if (time.time() - s) > 15:
+            break
+    if len(IPAddress()) == 1:
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    vpn_path = r"C:\Program Files\OpenVPN\bin\openvpn-gui.exe"
+    myvpn_dir = "proton"
+    myvpn = os.listdir(myvpn_dir)
+    # myvpn = [os.path.join(os.getcwd(), myvpn_dir, vpn) for vpn in myvpn if vpn.endswith(".ovpn")]
+
+    print("myvpn : ", myvpn)
+    print("IP Address : ", IPAddress())
+
+    VPN("connect", vpn_path, myvpn[4])
+    WaitUntilVPNConnected()
+
+    print("IP Address : ", IPAddress())
+
+    VPN("disconnect_all", vpn_path)
+    WaitUntilVPNDisconnected()
+
+    print("IP Address : ", IPAddress())
